b38e23fee083c0bd7164440d1d885c7f3993a1d2
Mohammed Saqlain <120318579+Sq1754@users.noreply.github.com>
2022-12-26 19:41:13 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Q3 {
+	public static void main(String[]args) {
+		
+		// Q3 : print Half pyramid
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Give number of rows:");
+		int rows = sc.nextInt();
+		
+		for(int i=1;i<=rows;i++) {
+			for(int j=1; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		// same thing in reverse  inverted pyramid
+		
+		System.out.println("\nInnverted pyramid:/n");
+		
+		for(int i=rows;i>=1;i--) {
+			for(int j=1; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		// pyramid for numbers:
+		
+		System.out.println("\nPyramid for numbers:\n");
+		for(int i=1;i<=rows;i++) {
+			for(int j=1; j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		// invertedd pyramid of numbers;
+		
+	System.out.println("\nInnverted pyramid for numbers:/n");
+		
+		for(int i=rows;i>=1;i--) {
+			for(int j=1; j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+}
